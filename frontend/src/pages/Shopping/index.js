bb9cff1375e47b7b49c831b60ea173493971a1fd
brenerovl <brener.oliveira@gec.inatel,br>
2019-08-16 00:47:27 -0300

    Inicio da pagina Shopping

--- frontend/src/pages/Shopping/index.js
+++ frontend/src/pages/Shopping/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './styles.css';
 
+import bag from '../../assets/Bag 2.svg';
+
 export default function Shopping() {
     return (
-        <div className="logo-container">
-            <h1>Passar Compra</h1>
+        <div className="shopping-container">
+            <form>
+                <input placeholder="Produto" />
+                <button type="submit">Add</button>
+            </form>
+            <div className="bag-container">
+                <img src={bag} alt="Sacola" height="68" />
+                <img src={bag} alt="Sacola" height="68" />
+            </div>
         </div>
     );
 }
