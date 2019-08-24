e7679991ce32840df187a65452d174461762480a
brenerovl <brener.oliveira@gec.inatel,br>
2019-08-24 19:07:05 -0300

    Bag's Algorithm Outline

--- frontend/src/pages/Shopping/index.js
+++ frontend/src/pages/Shopping/index.js
@@ -4,6 +4,24 @@ import './styles.css';
 import bag from '../../assets/bagFinal.svg';
 
 export default function Shopping() {
+    function bagCalculate(peso) {
+        let bags = [];
+        peso = 1000;
+        let pesoMax = 2000;
+        if(bags.size() === 0) {
+            bags.add(0);
+        }
+        for(let i = 0; i < bags.size(); i++){
+            if( (bags[i] += peso) <= pesoMax ) {
+                bags[i] += peso;
+            }else {
+                bags.add(peso);
+            }
+        }
+
+    }
+
+
     return (
         <div className="shopping-container">
             <form>
@@ -56,16 +74,69 @@ export default function Shopping() {
                             <img src={bag} alt="Sacola" height="68" />
                             <footer><strong>100%</strong></footer>
                         </li>
-                        <li>    
-                            <img src={bag} alt="Sacola" height="68" />
-                            <footer><strong>100%</strong></footer>
-                        </li>
+                        
                     </ul>
-                    <footer><strong>Total: 0</strong></footer>
+                    <div className="teste">
+                        <footer><strong>Total: 0</strong></footer>
+                    </div>
                 </div>
                 <div className="log-container">
                     <p>Produtos adicionados</p>
                     <ul>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
+                        <li>
+                            <p>produto 1</p><p>preço</p>
+                        </li>
                         <li>
                             <p>produto 1</p><p>preço</p>
                         </li>
