21a9ec3430ac7b5d0a23e49085f0c0c7a9bb0707
willersilva <willerrodrigo2006@hotmail.com>
2019-09-23 21:53:33 -0300

    Adicionando chartjs para as estatisticas
    Signed-off-by: willersilva <willerrodrigo2006@hotmail.com>

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Chart from 'chart.js';
+
 import './styles.css';
 
-import logo from '../../assets/logo.svg';
-
 export default function Home() {
+
+    useEffect(() => {
+        new Chart(document.getElementById("myChart"), {
+            type: 'line',
+            data: {
+              labels: [0,100,200,300,400,500,600],
+              datasets: [{ 
+                  data: [86,114,106,106,107,111,133],
+                  label: "Conventional Markets",
+                  borderColor: "#3e95cd",
+                  fill: false
+                }, {
+                  data: [168,170,178,230,203,276,408],
+                  label: "Green Markets",
+                  borderColor: "#3cba9f",
+                  fill: false
+                }
+              ]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Bags spended per kg (in 6 months)'
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Bags'
+                        }
+                    }],
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Kg'
+                        }
+                    }]
+                }
+            }
+          });
+    }, []);
+
     return (
-        <div className="logo-container">
-            <img src={logo} alt="Green Market" />
+        <div className="chart-container">
+            <canvas id="myChart" width="400" height="400"></canvas>
         </div>
     );
 }
